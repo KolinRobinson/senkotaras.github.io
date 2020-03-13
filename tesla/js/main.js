16b08698afc122e5c89463f9bef15e163c88f63c
Taras <taras.senko.99@gmail.com>
2020-03-13 23:26:05 +0200

    production

--- tesla/js/main.js
+++ tesla/js/main.js
@@ -1,3 +1,25 @@
 $(function(){
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        fade: true,
+        autoplay: 3000
+    });
 
+    var menuBtn = $('#menu-btn'),
+        menuNav = $('nav');
+    console.log(menuNav.attr('display'));
+    menuBtn.click(
+        function(){
+            this.className = (this.className == 'header-btn' ? 'close-btn' : 'header-btn')
+            if(this.className == 'header-btn'){
+                menuNav.removeClass("menu-active");
+                $("body").removeClass("fixed");
+                $("header").addClass("myIndex");
+            } else {
+                menuNav.addClass("menu-active");
+                $("body").addClass("fixed");
+                $("header").addClass("myIndex");
+            }
+        });
 });
